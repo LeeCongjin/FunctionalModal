8470d676d2109d82cd17c0524c56440e23b7f1fb
congjin.li <congjin.li@tusen.ai>
2023-08-15 18:16:28 +0800

    fix: 修改文件名，结构

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import ModalProvider from "./components/modals/ModalProvider.vue"
+import Button from "./components/Button.vue"
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <ModalProvider>
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+    <Button></Button>
+    随便放点东西
+  </ModalProvider>
 </template>
 
 <style scoped>
